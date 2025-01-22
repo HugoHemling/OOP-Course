4f98950e67b99aca34a2caa4c8e0ae066e6389fc
HugoHemling <hugo.hemling@edu.turkuamk.fi>
2025-01-22 19:27:26 +0200

    Task3 with sorted lists

--- oop_exercise1.py
+++ oop_exercise1.py
@@ -8,14 +8,20 @@ for i in range(10):
     number = int(input("Enter number: "))
     number_list.append(number)
 
-print("Enter 10 strings: ")
+print("\nEnter 10 strings: ")
 for i in range(10):
     string = input("Enter string: ")
     string_list.append(string)
 
-print("Number List: ", number_list)
-print("String List: ", string_list)
+print("\nNumber List: ", number_list)
+print("\nString List: ", string_list)
 
 number_list = [random.randint(1, 100) for i in range(10)]
 
-print("Randomly generated number list: ", number_list)
+print("\nRandomly generated number list: ", number_list)
+
+number_list.sort()
+string_list.sort()
+
+print("\nSorted numbers: ",number_list)
+print("Sorted string list: ",string_list)
