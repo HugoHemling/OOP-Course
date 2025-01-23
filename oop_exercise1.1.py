441a38ec5cc2fbd05a4ca94f03ee48e7daf43d0e
HugoHemling <hugo.hemling@edu.turkuamk.fi>
2025-01-23 16:01:16 +0200

    Task 5 even count

--- oop_exercise1.1.py
+++ oop_exercise1.1.py
@@ -1,14 +1,20 @@
-def count_negative_integers():
-    count = 0
+def count_integer_type():
+    neg_count = 0
+    even_count = 0 
+
     while True:
         num = int(input("Enter an integer: "))
         if num < 0:
-            count += 1
+            neg_count += 1
+        if num % 2 == 0:
+            even_count += 1
         if num == 0:
             break
 
-    print("Number of negative integers: ", count)
+    print("Number of negative integers: ", neg_count)
+    print("Number of even integers: ", even_count)
 
 
-count_negative_integers()
+count_integer_type()
+
 
