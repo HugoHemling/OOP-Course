cf0ee193606afa8e0e5a0b531842f536abbc382f
HugoHemling <hugo.hemling@edu.turkuamk.fi>
2025-01-23 16:09:01 +0200

    Task 6 div by 3 sum

--- oop_exercise1.1.py
+++ oop_exercise1.1.py
@@ -1,6 +1,7 @@
 def count_integer_type():
     neg_count = 0
     even_count = 0 
+    sum_pos_divby3 = []
 
     while True:
         num = int(input("Enter an integer: "))
@@ -8,11 +9,15 @@ def count_integer_type():
             neg_count += 1
         if num % 2 == 0:
             even_count += 1
+        if num > 0 and num % 3 == 0:
+            sum_pos_divby3.append(num)
         if num == 0:
             break
 
     print("Number of negative integers: ", neg_count)
     print("Number of even integers: ", even_count)
+    print("Number of positive integers divisible by 3: ", sum(sum_pos_divby3))
+
 
 
 count_integer_type()
